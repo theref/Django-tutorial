99e4749e3a5d1b17177eba25c80b971a4c10b56a
James Campbell <james.campbell@tanti.org.uk>
2014-02-01 11:42:31 +0000

    adds lots of functionality to admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from polls.models import Poll
-# Register your models here.
-admin.site.register(Poll)
+from polls.models import Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    list_display = ('question', 'pub_date', 'was_published_recently')
+
+admin.site.register(Poll, PollAdmin)
