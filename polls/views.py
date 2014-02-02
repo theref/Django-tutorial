502910721daecb2d4f0d111519fc9ab0f24d573a
James Campbell <james.campbell@tanti.org.uk>
2014-02-02 18:51:01 +0000

    adds 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import Http404
 from django.template import RequestContext, loader
 
 from polls.models import Poll
@@ -12,7 +13,11 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
